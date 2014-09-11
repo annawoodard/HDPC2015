5d10e609367f21a67431fe036ce903e342fcfc40
Anna Elizabeth Woodard <awoodard@nd.edu>
2014-09-11 08:51:57 -0400

    Adding ACM template skeleton and example.

---
+++ compile.sh
@@ -0,0 +1,5 @@
+latex HDPC2015
+latex HDPC2015
+bibtex HDPC2015
+latex HDPC2015
+dvipdfm HDPC2015.dvi
---
+++ example/compile.sh
@@ -0,0 +1,5 @@
+latex sigproc-sp
+latex sigproc-sp
+bibtex sigproc
+latex sigproc-sp
+dvipdfm sigproc-sp.dvi
